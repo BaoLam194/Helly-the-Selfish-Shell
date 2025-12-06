6da93cda15b5621f240f42dbe4d31fdc00bcc9ca
anotherBL <tranbaolam006@gmail.com>
2025-12-06 12:36:40 +0700

    chore: all commands are temporarily invalid!

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
-
+  char input[512];
+  fgets(input, sizeof(input), stdin);
+  // Remove trailing end-line and add null byte
+  input[strcspn(input, "\n")] = '\0';
+  printf("%s: command not found\n", input);
   return 0;
 }
