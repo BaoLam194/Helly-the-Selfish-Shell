f6d0857d4153cea49bca08ad0b32e0aa47608e86
anotherBL <tranbaolam006@gmail.com>
2025-12-06 14:24:23 +0700

    feat: rudimentary exit command

--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@ int main(int argc, char *argv[]) {
     fgets(input, sizeof(input), stdin);
     // Remove trailing end-line and add null byte
     input[strcspn(input, "\n")] = '\0';
+    if (strcmp(input, "exit") == 0)
+      break;
     printf("%s: command not found\n", input);
   }
   return 0;
